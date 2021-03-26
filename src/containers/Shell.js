89c474ff51348f224df656398c25b939b6ba4000
ezza022 <alfirman.pahlepi@gmail.com>
2021-03-26 15:34:44 +0700

    add level, redesign

--- src/containers/Shell.js
+++ src/containers/Shell.js
@@ -4,10 +4,14 @@ import { Cup } from "../components/Cup";
 import "./Shell.css";
 
 const Shell = () => {
+  const easy = "duration-700";
+  const medium = "duration-500";
+  const hard = "duration-300";
+
   const [box1, setBox1] = useState("");
   const [box2, setBox2] = useState("");
   const [box3, setBox3] = useState("");
-  const [level, setLevel] = useState("easy");
+  const [level, setLevel] = useState(easy);
   const [isShuffle, setShuffle] = useState(false);
   const [isPlay, setPlay] = useState(false);
 
@@ -18,8 +22,22 @@ const Shell = () => {
   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
   async function animate() {
-    for (var i = 0; i <= 10; i++) {
-      await timer(800);
+    for (var i = 0; i <= 15; i++) {
+      let delay = 0;
+      switch (level) {
+        case easy:
+          delay = 700;
+          break;
+        case medium:
+          delay = 500;
+          break;
+        case hard:
+          delay = 300;
+          break;
+        default:
+          break;
+      }
+      await timer(delay);
       let random = Math.random();
 
       //sorry badcode, i'll refactoring soon -_-
@@ -47,8 +65,8 @@ const Shell = () => {
       } else if (random > 0.6) {
         if (box1 !== "right" && box2 !== "left" && box3 !== "mid") {
           setBox1("right");
-          setBox2("mid");
-          setBox3("left");
+          setBox2("left");
+          setBox3("mid");
         } else {
           setBox1("left");
           setBox2("mid");
@@ -67,13 +85,37 @@ const Shell = () => {
 
   return (
     <div className="app h-screen w-screen flex bg-gray-600 text-white">
-      <div className="container p-10 h-full max-w-full w-3/4 bg-gray-700  flex justify-center items-center">
-        <div className="space-y-36">
-          <div className="title w-full space-y-2 ">
-            <h1 className="font-bold text-5xl text-center">Shell Game</h1>
-            <h2 className="ml-1">By ezza022</h2>
+      <div className="container h-full w-11/12 md:w-3/4 bg-gray-700  flex flex-wrap justify-center content-between py-10">
+        <div className="title w-full flex justify-center items-center ">
+          <div className="space-y-2">
+            <h1 className="font-bold text-5xl text-center w-full">
+              Shell Game
+            </h1>
+            <h2 className="ml-2">By ezza022</h2>
           </div>
-          <div className=" content w-52 m-auto relative">
+        </div>
+        <div className="space-y-36">
+          <div className="flex justify-center items-center space-x-5">
+            <div onClick={() => setLevel(easy)} className="space-x-1">
+              <input
+                type="radio"
+                id="easy"
+                name="level"
+                value="easy"
+                defaultChecked
+              />
+              <label htmlFor="easy">Easy</label>
+            </div>
+            <div onClick={() => setLevel(medium)} className="space-x-1">
+              <input type="radio" id="medium" name="level" value="medium" />
+              <label htmlFor="medium">Medium</label>
+            </div>
+            <div onClick={() => setLevel(hard)} className="space-x-1">
+              <input type="radio" id="hard" name="level" value="hard" />
+              <label htmlFor="hard">Hard</label>
+            </div>
+          </div>
+          <div className="w-52 m-auto relative">
             <Cup
               isShuffle={isShuffle}
               isPlay={isPlay}
@@ -106,6 +148,7 @@ const Shell = () => {
             handleClick={handleClick}
           />
         </div>
+        <div className="w-full">score</div>
       </div>
     </div>
   );
