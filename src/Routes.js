7b30e838d36f54d4e943fef79ee3b855744b9413
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2022-05-05 15:53:12 +0700

    feat: classic mode

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import { createContext, useContext, useState } from "react";
 import { Routes as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Leaderboard from "./pages/Leaderboard";
-import History from "./pages/History";
-import Setting from "./pages/Setting";
+// import Leaderboard from "./pages/Leaderboard";
+// import History from "./pages/History";
+// import Setting from "./pages/Setting";
 import Layout from "./components/Layout";
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navigation";
 
 const Cheat = createContext();
 
@@ -13,13 +13,13 @@ const Routes = () => {
   const [showJackpot, setShowJackpot] = useState(false);
   return (
     <Layout>
-      <Navigation />
+      {/* <Navigation /> */}
       <Cheat.Provider value={{ showJackpot, setShowJackpot }}>
         <Router>
           <Route path="/" element={<Home />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
+          {/* <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/history" element={<History />} />
-          <Route path="/setting" element={<Setting />} />
+          <Route path="/setting" element={<Setting />} /> */}
         </Router>
       </Cheat.Provider>
     </Layout>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Button } from "../components/Button";
 import Cup from "../components/Cup";
 import InputRadioLabel from "../components/InputRadioLabel";
-import Modal from "../components/Modal";
+// import Modal from "../components/Modal";
 import useShellGame from "../hooks/useShellGame";
 
 const Home = () => {
-  const [modal, setModal] = useState(false);
+  // const [modal, setModal] = useState(false);
   const { score, shuffle, userChoice, setLevel, status, cups } = useShellGame();
 
   return (
     <>
-      {modal && <Modal score={score} setModal={setModal} />}
+      {/* {modal && <Modal score={score} setModal={setModal} />}
       <div className="title w-full flex justify-center items-center z-0">
         <div className="space-y-2">
           <h1 className="font-bold text-5xl lg:text-7xl text-center w-full">
@@ -26,47 +26,52 @@ const Home = () => {
             By alfirman pahlepi
           </a>
         </div>
-      </div>
-      <div className="space-y-36">
-        <div className="flex justify-center items-center space-x-5">
-          {radios.map((radio, index) => (
-            <InputRadioLabel
-              key={index}
-              level={radio.level}
-              color={radio.color}
-              value={radio.level}
-              defaultChecked={radio.defaultChecked}
-              onChange={(e) => setLevel(e.target.value)}
+      </div> */}
+      <div className="flex items-center justify-center h-full">
+        <div className="space-y-36">
+          <div className="flex justify-center items-center space-x-5">
+            {radios.map((radio, index) => (
+              <InputRadioLabel
+                key={index}
+                level={radio.level}
+                color={radio.color}
+                value={radio.level}
+                defaultChecked={radio.defaultChecked}
+                onChange={(e) => setLevel(e.target.value)}
+                disabled={status === "shuffling" || status === "choosing"}
+              >
+                {radio.level}
+              </InputRadioLabel>
+            ))}
+          </div>
+          <div className="relative flex justify-center items-center">
+            {cups.map((cup, index) => (
+              <Cup
+                key={index}
+                cup={cup}
+                status={status}
+                userChoice={userChoice}
+              />
+            ))}
+          </div>
+          <div className="w-full text-center">
+            <Button
               disabled={status === "shuffling" || status === "choosing"}
+              onClick={shuffle}
             >
-              {radio.level}
-            </InputRadioLabel>
-          ))}
-        </div>
-        <div className="relative flex justify-center items-center">
-          {cups.map((cup, index) => (
-            <Cup
-              key={index}
-              cup={cup}
-              status={status}
-              userChoice={userChoice}
-            />
-          ))}
-        </div>
-        <div className="w-full text-center">
-          <Button
-            disabled={status === "shuffling" || status === "choosing"}
-            onClick={shuffle}
-          >
-            {status === "ready" && "Play"}
-            {status === "over" && "Game over, Play again?"}
-            {status === "shuffling" && "Shuffling..."}
-            {status === "choosing" && "Where is the yellow ball?"}
-          </Button>
+              {status === "ready" && "Play"}
+              {status === "over" && "Game over, Play again?"}
+              {status === "shuffling" && "Shuffling..."}
+              {status === "choosing" && "Where is the yellow ball?"}
+            </Button>
+          </div>
+          <h2 className="text-3xl text-center md:text-5xl">
+            Score : <span className="font-bold">{score}</span>
+          </h2>
         </div>
       </div>
 
-      <div className="w-full mb-10 space-y-8 ">
+      {/* <div className="w-full mb-10 space-y-8 ">
         <div className="text-center">
           {status === "over" && score && (
             <button
@@ -80,7 +85,7 @@ const Home = () => {
         <h2 className="text-3xl text-center md:text-5xl">
           Score : <span className="font-bold">{score}</span>
         </h2>
-      </div>
+      </div> */}
     </>
   );
 };
